acb926516ce9e66499550869b0386ecce6c35c45
jvaresi <jvaresi@beeside.fr>
2019-11-21 11:26:17 +0100

    rattrapage

---
+++ sw.js
@@ -0,0 +1,67 @@
+const cacheName = 'veille-techno' + '1.1';
+
+self.addEventListener('install', (evt) => {
+    console.log(`sw installé à ${new Date().toLocaleTimeString()}`);
+
+    // 4.4 Gestion du cache par le SW
+    const cachePromise = caches.open(cacheName).then(cache => {
+        return cache.addAll([
+            'index.html',
+            'main.js',
+            'style.css',
+            'vendors/bootstrap4.min.css',
+            'add_techno.html',
+            'add_techno.js',
+            'contact.html',
+            'contact.js',
+        ])
+        .then(console.log('cache initialisé'))
+        .catch(console.err);
+    });
+
+    evt.waitUntil(cachePromise);
+
+});
+
+self.addEventListener('activate', (evt) => {
+    console.log(`sw activé à ${new Date().toLocaleTimeString()}`);    
+});
+
+self.addEventListener('fetch', (evt) => {
+    if(!navigator.onLine) {
+        const headers = { headers: { 'Content-Type': 'text/html;charset=utf-8'} };
+        evt.respondWith(new Response('<h1>Pas de connexion internet</h1><div>Application en mode dégradé. Veuillez vous connecter</div>', headers));
+    }
+
+    // console.log('sw intercepte la requête suivante via fetch', evt);
+    console.log('url interceptée', evt.request.url);
+
+
+    // 5.1 Stratégie : cache only with network callback
+    evt.respondWith(
+        caches.match(evt.request)
+            .then(cachedResponse => {   
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                // 5.1 Stratégie de cache
+                return fetch(evt.request).then(
+                    // On récupère la requête
+                    function(newResponse) {
+                        // Accès au cache
+                        caches.open(cacheName).then(
+                            function(cache){
+                                // ajout du résultat de la requête au cache
+                                cache.put(evt.request, newResponse);
+                            }
+                        );
+                        // Utilisation de clone car on ne peut utiliser qu'une fois la réponse
+                        return newResponse.clone();
+                    }
+                )
+            }
+        )
+    );
+});
+
